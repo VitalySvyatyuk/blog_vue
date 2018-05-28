814e30c219afd3c166e23e5f694d1f059dca40cb
Vitaly Svyatyuk <goodgame1945@gmail.com>
2018-05-28 09:01:42 +0300

    before Vue

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from articles.views import ArticlesView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ArticlesView.as_view(), name='home')
 ]
