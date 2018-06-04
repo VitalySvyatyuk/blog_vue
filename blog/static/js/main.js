9998737d18bdcd4f50ab70028b27c06aaa04472f
Vitaly Svyatyuk <goodgame1945@gmail.com>
2018-06-04 11:40:13 +0300

    fetch data from API

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -1,7 +1,46 @@
-var app = new Vue({
+// delimiters: ['[[', ']]'],
+// var example2 = new Vue({
+//   delimiters: ['[[', ']]'],
+//   el: '#example-2',
+//   data: {
+//     parentMessage: 'Parent',
+//     items: [
+//       { message: 'Foo' },
+//       { message: 'Bar' }
+//     ]
+//   }
+// });
+//
+// var example3 = new Vue({
+//   delimiters: ['[[', ']]'],
+//   el: '#example-3',
+//   data: {
+//   }
+// })
+
+Vue.component('blog-post', {
+  props: ['title', 'body'],
+  template: `
+    <div class="blog-post">
+      <h3>{{ title }}</h3>
+      <label>{{ body }}</label>
+    </div>`
+})
+
+new Vue({
   delimiters: ['[[', ']]'],
-  el: '#app',
+  el: '#blog-post-demo',
   data: {
-    message: 'Hello Vue!'
+    posts: []
+  },
+  created: function () {
+    var vm = this
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(function (response) {
+        return response.json()
+      })
+      .then(function (data) {
+        vm.posts = data
+      })
   }
-});
+})
